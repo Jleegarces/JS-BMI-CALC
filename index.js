637e0e665e6f27fef658b710eb593e8f056f80e3
Jleegarces <86203137+Jleegarces@users.noreply.github.com>
2021-12-19 12:29:46 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function bmiCalculator(weight, height) {
+    var weight = prompt("what is your weight in kg?");
+    var height = prompt("what is your height in m ");
+
+    var bmi = Math.round(weight / height **2)
+    return ('your bmi is: ' + bmi);
+    
+
+
+}
+
+bmiCalculator()
+
+
+    
+    
+    
+    
+
+
+
+
